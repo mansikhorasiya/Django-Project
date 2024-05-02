6014a6ff634eaee9428f0e7be34a7ca2fe1e2cbe
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2024-05-02 11:42:23 +0530

    use the template for loop

--- cubetech/cubetech/views.py
+++ cubetech/cubetech/views.py
@@ -2,7 +2,17 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def homePage(request):
-    return render(request,"index.html")
+        data={
+             'title':'Home new',
+             'bdata':'Welcome to cubetech || Jodhpur',
+             'clist':['python' , 'java' , 'php'],
+             'StudentDetails':[
+                  {'name':'Testing1','phone':9876547895},
+                  {'name':'Testing2','phone':9888765478}
+             ]
+        }
+
+        return render(request,"index.html",data)
 
 def aboutUs(request):
     return HttpResponse("Welcome to Cubetech..")
