2a759ec9b0b1ccf43e3821f001197153699c75ca
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2024-05-09 12:55:30 +0530

    creating consumers,connect and disconnect methods
    crreating URL for ChatConsumer

---
+++ ChatProject/chatprj/ChatApp/consumers.py
@@ -0,0 +1,38 @@
+import json
+from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
+from ChatApp.models import *
+
+class ChatConsumer(AsyncWebsocketConsumer):
+
+    async def connect(self):
+        self.room_name=f"room_{self.scope['url_route']['kwargs']['room_name']}"
+        await self.channels_layer.group_add(self.room_name,self.channel_name)
+        await self.accept()
+
+        print('connected')
+
+    async def disconnect(self,close_code):
+        await self.channels_layer.group_add(self.room_name,self.channel_name)
+
+    # async def receive(self,text_data):
+    #     text_data_json = json.loads(text_data)
+    #     message = text_data_json
+
+    #     print(message)
+
+    # async def send_message(self, event):
+    #     data =event['message']
+    #     await self.create_message(data=data)
+    #     respons_data={
+    #         'sender':data['sender'],
+    #         'message':data['message']
+    #     }
+    #     await self.send(text_data=json.dumps({'message': respons_data}))
+
+    # @database_sync_to_async
+    # def create_message(self,data):
+    #     get_room_by_name=Room.objects.get(room_name=data['room_name'])
+    #     if not Message.objects.filter(message=data['message']).exists():
+    #         new_message=Message(room=get_room_by_name,sender=data['sender'],message=data['message'])
+    #         new_message.save()
