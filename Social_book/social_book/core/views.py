2a93a8f5caefa9ca5e932f1841a20e549895b338
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2024-07-24 16:38:26 +0530

    Sign-Up

--- Social_book/social_book/core/views.py
+++ Social_book/social_book/core/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User , auth
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return render(request,'index.html')
 
-
+def signup(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+    
+    else:
+        return render(request, 'signup.html')
