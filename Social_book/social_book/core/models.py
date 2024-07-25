f8b44fce0a98aeba0e5e9e279a7acd60f447ce75
mansikhorasiya <148621411+mansikhorasiya@users.noreply.github.com>
2024-07-25 17:03:00 +0530

    create a for new user (signup)

--- Social_book/social_book/core/models.py
+++ Social_book/social_book/core/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from django.contrib.auth import get_user_model
 
-User = get_user_model()   #Model
+User = get_user_model()
+
 # Create your models here.
-class profile(models.Model):
+class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField()
-    bio =models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to='profile_iamges', default='blank-profile-picture.png')
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_images', default='blank-profile-picture.png')
     location = models.CharField(max_length=100, blank=True)
 
     def __str__(self):
